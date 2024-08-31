6c149d6735a9e5fb528405274917f77f96bc1860
Marc Mansour <marcm9387@gmail.com>
2024-08-31 23:30:20 +0200

    amending triggerfunction and regions

--- src/functions/src/triggers/triggerDailySync.ts
+++ src/functions/src/triggers/triggerDailySync.ts
@@ -1,11 +1,23 @@
 import * as functions from "firebase-functions";
 
 export const triggerDailySync = functions.pubsub
-  .schedule("0 0 * * *")
+  .schedule("0 12 * * *")
   .timeZone("Europe/Paris")
   .onRun(async (context) => {
-    const region = process.env.FUNCTION_REGION;
-    const fullSyncUrl = `https://${region}-${process.env.PROJECT_ID}.cloudfunctions.net/fullSyncToolsToTypesense`;
-    await fetch(fullSyncUrl);
-    return null;
+    console.log("triggerDailySync function started");
+    try {
+      const region = functions.config().project.region;
+      const projectId = functions.config().project.id;
+      console.log("Region:", region);
+      console.log("Project ID:", projectId);
+      const fullSyncUrl = `https://${region}-${projectId}.cloudfunctions.net/fullSyncToolsToTypesense`;
+      console.log("Full sync URL:", fullSyncUrl);
+      const response = await fetch(fullSyncUrl);
+      console.log("Fetch response status:", response.status);
+      console.log("Fetch response text:", await response.text());
+      return null;
+    } catch (error) {
+      console.error("Error in triggerDailySync:", error);
+      throw error;
+    }
   });
